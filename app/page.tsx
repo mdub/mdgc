b2e998769028eb0399a585272540d60917a36195
Mike Williams <mdub@dogbiscuit.org>
2024-07-28 19:00:36 +1000

    A pointless change to test previews.

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         />
       </div>
 
-      Yo mama.
+      Yo mama is funky.
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
